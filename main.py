240e9a587e3fb6d7b6724c77f5b33a6c2be74129
206 <206@academicos.cesde.edu.co>
2022-02-25 20:42:18 -0500

    NTV001 se crea  funcion que dice los niveles de agua y se imprime el mensaje dependiendo del nivel

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 
-print ("Hola Mundo")
-
 nivelAgua= int(input("Digite la cantidad de agua de la represa: "))
 print(f"El nivel de agua es: {nivelAgua}")
 
-print ("oe")
+if (nivelAgua<200):
+    print(f"el nivel de agua es bajo")
+elif (nivelAgua>=200 and nivelAgua<450):
+    print(f"Niveles de agua estables")
+else:
+    print(f"Necesario abrir las compuertas...")
